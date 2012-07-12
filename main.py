e29ec033ad49ecf74f1b8d70d202cab26016b625
Josh Zimmerman <zimagic@gmail.com>
2012-07-12 16:31:50 -0700

    Main driver for program

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import time
+
+import github_reports
+import google_code_reports
+
+while True:
+    github_reports.main()
+    google_code_reports.main()
+    print("Sleeping...")
+    time.sleep(60*10)
