05a580a4e8ed57c249148f2f30606c73a93a2d90
Josh Zimmerman <zimagic@gmail.com>
2012-07-24 14:46:32 -0700

    Fixed google_code_reports to not assume that every period is of equal length.

--- google_code_reports.py
+++ google_code_reports.py
@@ -1,19 +1,24 @@
 import copy
 import json
+import time
 import urllib2
 
 import hipchat_message
 
+# Threshold of average to report an elevated rate
 change_threshold = 1.10
 
 
 def get_errors(old_reports):
+    '''Given an old dict (contents of google_code), get new issues from Google
+    Code and return an updated version of the dict with information about
+    the new bug reports.
+    '''
 
-    last_id = old_reports["last_id"]
-
+    # Use a list because otherwise we couldn't access issue_count in get_issues
     issue_count = [0]
 
-    def get_issues(page, last_id):
+    def get_issues(page, stop_id):
         request = urllib2.urlopen(
             "http://code.google.com/p/khanacademy/issues/csv?can=2&q=&colspec=ID&sort=-ID&start=%d"
             % (page * 100))
@@ -22,32 +27,41 @@ def get_errors(old_reports):
 
         # Parse the CSV file
         issues = issues.split("\n")
+        # Ignore trailing commas
+        issues = [issue.rstrip(",") for issue in issues]
         issues = [issue.split(",") for issue in issues]
         issues = issues[1:]  # Ignore column headers
         issues = issues[:-2]  # Strip junk from the end
-        issues = [int(x[0].replace('"', '')) for x in issues]
-        # Make sure we cut off at the right point
-        issues = sorted(issues, reverse=True)
+        issues = [int(issue[0].replace('"', '')) for issue in issues]
+        # Note: We don't need to explicitly sort here because the request
+        # to Google specified we sort by -ID
 
         should_continue = True
         for issue in issues:
-            if issue > last_id:
+            if issue > stop_id:
                 issue_count[0] += 1
             else:
                 should_continue = False
                 break
 
-        if should_continue and last_id != -1:
-            get_issues(page + 1, last_id)
+        if should_continue and stop_id != -1:
+            get_issues(page + 1, stop_id)
 
-        return max(issues[0], last_id)
+        return max(issues[0], stop_id)
 
-    first_id = get_issues(0, last_id)
+    max_id = get_issues(0, old_reports["last_id"])
     issue_count = issue_count[0]
 
-    old_reports["last_id"] = first_id
+    cur_time = time.time()
+    time_this_period = cur_time - old_reports["last_time"]
+
+    old_reports["last_id"] = max_id
     old_reports["issue_count"] += issue_count
+    old_reports["elapsed_time"] += time_this_period
+    old_reports["last_time"] = cur_time
+    # Just for temporary use
     old_reports["issues_this_period"] = issue_count
+    old_reports["time_this_period"] = time_this_period
 
     return old_reports
 
@@ -58,20 +72,32 @@ def main():
         old_reports = json.loads(google_code_file.read())
     except IOError:
         google_code_file = open("google_code", 'w')
-        old_reports = {"num_periods": 0,
+        old_reports = {"elapsed_time": 0,
                        "last_id": -1,
                        "issue_count": 0,
-                       "issues_this_period": 0}
+                       "last_time": 0}
 
-    old_reports["num_periods"] += 1
     new_reports = get_errors(copy.deepcopy(old_reports))
 
-    old_rate = old_reports["issue_count"] / old_reports["num_periods"]
-    if (old_rate != 0 and
-            change_threshold * old_rate < new_reports["issues_this_period"]):
+    old_rate = old_reports["issue_count"] / old_reports["elapsed_time"]
+    new_rate = (new_reports["issues_this_period"] /
+                    new_reports["time_this_period"])
+
+    if (old_rate != 0 and change_threshold * old_rate < new_rate):
         # Too many errors!
         hipchat_message.send_message(
-            "Elevated bug report rate on <a href='http://code.google.com/p/khanacademy/issues/'>Google code!</a>")
+            "Elevated bug report rate on"
+            " <a href='http://code.google.com/p/khanacademy/issues/'>Google"
+            " code!</a>"
+            " Current rate: %.3f per hour. Average rate: %.3f per hour."
+            " (Current is %.2f%% of average)"
+            % (new_rate * 60 * 60,
+               old_rate * 60 * 60,
+               (new_rate / old_rate) * 100))
+
+    # Delete fields we don't need anymore
+    del(new_reports["issues_this_period"])
+    del(new_reports["time_this_period"])
 
     google_code_file.seek(0)
     google_code_file.truncate()
